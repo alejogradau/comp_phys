32a4159b7094c469d6b72dfe982bb8349612b33f
Ricardo Zamora <iyttahm@me.com>
2020-11-28 22:07:57 +0100

    Add option for plotting time averages

--- H1_ricardo/src/plotter.py
+++ H1_ricardo/src/plotter.py
@@ -12,6 +12,8 @@ import matplotlib.pyplot as plt
 energy = True
 temperature = True
 pressure = True
+temperature_avg = True
+pressure_avg = True
 
 if energy:
     array = np.genfromtxt('./output/energy.csv', delimiter=',', skip_header=1)
@@ -20,7 +22,7 @@ if energy:
     ax.plot(array[:, 0], array[:, 1], label="Kinetic Energy")
     ax.plot(array[:, 0], array[:, 2], label="Potential Energy")
     ax.plot(array[:, 0], array[:, 3], label="Total Energy")
-#    ax.set_title('FCC Lattice')
+    ax.set_title('N-particle system time-dependent energy')
     ax.set_xlabel('Time (ps)')
     ax.set_ylabel('Energy (eV)')
     plt.legend()
@@ -32,7 +34,7 @@ if temperature:
 
     fig, ax = plt.subplots(figsize=(10,4))
     ax.plot(array[:, 0], array[:, 1], label="")
-#    ax.set_title('FCC Lattice')
+    ax.set_title('N-particle system')
     ax.set_xlabel('Time (ps)')
     ax.set_ylabel('Temperature (ºK)')
     plt.legend()
@@ -44,9 +46,33 @@ if pressure:
 
     fig, ax = plt.subplots(figsize=(10,4))
     ax.plot(array[:, 0], array[:, 1], label="")
-#    ax.set_title('FCC Lattice')
+    ax.set_title('N-particle system')
     ax.set_xlabel('Time (ps)')
-    ax.set_ylabel('Pressure (??)')
+    ax.set_ylabel('Pressure (bar)')
     plt.legend()
 
     fig.savefig('./output/pressure.pdf')
+
+if temperature_avg:
+    array = np.genfromtxt('./output/temperature_avg.csv', delimiter=',', skip_header=1)
+
+    fig, ax = plt.subplots(figsize=(10,4))
+    ax.plot(array[:, 0], array[:, 1], label="")
+    ax.set_title('N-particle system Time Average')
+    ax.set_xlabel('Time (ps)')
+    ax.set_ylabel('Temperature (ºK)')
+    plt.legend()
+
+    fig.savefig('./output/temperature_avg.pdf')
+
+if pressure_avg:
+    array = np.genfromtxt('./output/pressure_avg.csv', delimiter=',', skip_header=1)
+
+    fig, ax = plt.subplots(figsize=(10,4))
+    ax.plot(array[:, 0], array[:, 1], label="")
+    ax.set_title('N-particle system Time Average')
+    ax.set_xlabel('Time (ps)')
+    ax.set_ylabel('Pressure (bar)')
+    plt.legend()
+
+    fig.savefig('./output/pressure_avg.pdf')
