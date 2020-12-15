796617736b81c33add28dfca7bbe4a758f2f1e77
Alejandro Grajales <agrajalesd@unal.edu.co>
2020-12-15 07:04:34 -0500

    The plots show that the sampled points during the Metropolis algorithm and the radial probability density look very similar for the case of Z=2. I started writing everything in the task2/ directory.

--- H2_alejo/task2/src/H2main.c
+++ H2_alejo/task2/src/H2main.c
@@ -20,10 +20,12 @@
  #include <stdio.h>   //printf
 
  /******************************************************************************
-  * Helper functions
+  * Helper functions header
   *****************************************************************************/
- void mc_integration_metropolis(unsigned int N, double alpha);
- double gen_trial_change(double accepted, double r);
+  unsigned int mc_integration_metropolis(unsigned int N,
+               double alpha, double burn_factor, double d,
+               double *conf_m, double *pos_large);
+ double gen_trial_change(double accepted, double rn, double d);
  double vector_magnitude(double x, double y, double z);
  double relative_prob(double x1_m, double y1_m, double z1_m,
    double x2_m, double y2_m, double z2_m,
@@ -35,60 +37,134 @@
    double z1, double z2, double alpha);
  void calc_autocorrelation(unsigned long length, double time_series[length],
    double autocorrelation[length/2]);
- void file_to_array(unsigned int length, double data[length]);
+ void file_to_array(char *fname, unsigned int length, double data[length]);
  void calc_block_average(unsigned long length, double time_series[length],
    double block_average[length/2]);
+void slice_array(unsigned int N, double pos_large[],
+                 unsigned int n_accepted, double *pos);
+void int_array_to_file(char *fname, int length, int *array);
+void array_to_file(char *fname, int length, double *array);
+double map_to_int(unsigned int n_accepted, double pos[n_accepted],
+                int *int_pos, int n_bins);
+void build_histogram(unsigned int numvalues, int *values);
+void radial_density_file(char *fname, double bin_size, int n_bins, double Z);
 
-
-
-
-
+/******************************************************************************
+ * MAIN
+ *****************************************************************************/
 int main(int argc, char *argv[])
 {
-  // Define parameters
+  // Initialize Variables
   double alpha = 0.1;
-  mc_integration_metropolis(1e5, alpha);
+  unsigned int N = 1e6;
+  unsigned int n_accepted;
+  int n_bins = 20;
+  double bin_size;  // Returned by map_to_int() function
+  double burn_factor = 0.1;
+  double burn_period = burn_factor*N;  //"Burn-in" is burn_factor% of total run
+  double d = 1.5;  // symmetric displacement generated in gen_trial_change
+  double Z = 27/16;
+
+  // Memory allocation
+  double *conf_m = calloc(6, sizeof(double));  // Configuration m coordinates
+  double *pos_large = calloc(N/2.0, sizeof(double)); // Electron1's rad position. TOO LARGE (N/2)
+
+  // File names
+  //char fhistogram[20] = "./out/histogram.csv";
+
+  conf_m[0] = 2000.0;
+  conf_m[1] = 2000.0;
+  conf_m[2] = 2000.0;
+  conf_m[3] = -2000.0;
+  conf_m[4] = -2000.0;
+  conf_m[5] = -2000.0;
+  printf("Electron 1 initial coordinates (%f,%f,%f)\n",
+          conf_m[0], conf_m[1], conf_m[2]);
+  printf("Electron 2 initial coordinates (%f,%f,%f)\n",
+          conf_m[3], conf_m[4], conf_m[5]);
+
+  n_accepted = mc_integration_metropolis(N, alpha, burn_factor, d,
+                                         conf_m, pos_large);
+  printf("Metropolis done\n");
+  printf("n_accepted = %u\n", n_accepted);
+
+  double *pos = calloc(n_accepted, sizeof(double)); // E1's rad position.
+  int *int_pos = calloc(n_accepted, sizeof(int)); // E1's rad int position.
+  printf("Memory allocated\n");
+
+  //file_to_array("./out/pos_large.csv", n_accepted, pos);
+  //printf("file_to_array OK\n");
+
+  printf("Slicing array\n");
+  slice_array(N, pos_large, n_accepted, pos);
+  free(pos_large);
+  printf("Array sliced\n");
+
+  //printf("array_to_file ...\n");
+  //array_to_file("./out/pos.csv", n_accepted, pos);
+  //printf("array_to_file OK\n");
+
+  bin_size = map_to_int(n_accepted, pos, int_pos, n_bins);
+  printf("Array mapped to integers\n");
+
+  build_histogram(n_accepted, int_pos);
+  radial_density_file("./out/radial_density.csv", bin_size, n_bins, Z);
+
+  printf("Metropolis integration for N=%d\n", N);
+  printf("Accepted steps (excluding burn-in period): %d\n", n_accepted);
+  printf("Acceptance rate:            %f%%\n", n_accepted/(N-burn_period)*100);
+  printf("Look above for E expectation value, sigma_E, and sigma_n\n");
 }
 
-void mc_integration_metropolis(unsigned int N, double alpha)
+/******************************************************************************
+ * Helper functions
+ *****************************************************************************/
+
+/* Performs Monte Carlo integration using the Metropolis algorithm
+ * @ N - Number of time steps
+ * @ alpha - Trial wave function parameter
+ * @ conf_m - Coordinates of the configuration m (2 electrons)
+ * @ pos_large[N/2] - Array of electron 1's radial position. TOO LARGE (N/2)
+ * @ N/2: acceptance rate can be up to 50%
+ */
+ unsigned int mc_integration_metropolis(unsigned int N,
+              double alpha, double burn_factor, double d,
+              double *conf_m, double *pos_large)
 {
     unsigned int n_accepted = 0;
-    double burn_period = 0.5*N; //"Burn-in" period set to 1% of total run
-    // current configurations (m)
-    double x1_m, y1_m, z1_m, x2_m, y2_m, z2_m;
-    // trial configurations (t)
-    double x1_t, y1_t, z1_t, x2_t, y2_t, z2_t;
+    double burn_period = burn_factor*N;  //"Burn-in" is burn_factor% of total run
     double E_i = 0, E_mean = 0, E2_mean = 0, sigma_E, sigma_n;
     FILE *fp = fopen("./out/local_energy.csv", "w");
+    fprintf(fp, "time, local energy\n");
+    FILE *gp = fopen("./out/pos_large.csv", "w");
+    fprintf(gp, "time, position\n");
+
+    // Declare Variables
+    double x1_m, y1_m, z1_m, x2_m, y2_m, z2_m;  // current configurations (m)
+    double x1_t, y1_t, z1_t, x2_t, y2_t, z2_t;  // trial configurations (t)
+
+    // Electron 1 initial positions
+    x1_m = conf_m[0];
+    y1_m = conf_m[1];
+    z1_m = conf_m[2];
+    // Electron 2 initial positions
+    x2_m = conf_m[3];
+    y2_m = conf_m[4];
+    z2_m = conf_m[5];
 
     srand(time(NULL));
-
-    // Generate values for initial guesses
-    // Electron 1
-    x1_m = 2000.0;
-    y1_m = 2000.0;
-    z1_m = 2000.0;
-    printf("Electron 1 initial coordinates (%f,%f,%f)\n", x1_m, y1_m, z1_m);
-
-    // Electron 2
-    x2_m = -2000.0;
-    y2_m = -2000.0;
-    z2_m = -2000.0;
-    printf("Electron 1 initial coordinates (%f,%f,%f)\n", x2_m, y2_m, z2_m);
-
-    fprintf(fp, "time, local energy\n");
     for (unsigned long i = 0; i < N; i++)
     {
         //Generates trial changes based on the current accepted values
         // Electron 1
-        x1_t = gen_trial_change(x1_m, rand_num);
-        y1_t = gen_trial_change(y1_m, rand_num);
-        z1_t = gen_trial_change(z1_m, rand_num);
+        x1_t = gen_trial_change(x1_m, rand_num, d);
+        y1_t = gen_trial_change(y1_m, rand_num, d);
+        z1_t = gen_trial_change(z1_m, rand_num, d);
 
         // Electron 2
-        x2_t = gen_trial_change(x2_m, rand_num);
-        y2_t = gen_trial_change(y2_m, rand_num);
-        z2_t = gen_trial_change(z2_m, rand_num);
+        x2_t = gen_trial_change(x2_m, rand_num, d);
+        y2_t = gen_trial_change(y2_m, rand_num, d);
+        z2_t = gen_trial_change(z2_m, rand_num, d);
 
         //Calculates the relative probability q = p_t/p_m
         double q = relative_prob(x1_m, y1_m, z1_m, x2_m, y2_m, z2_m,
@@ -116,14 +192,23 @@ void mc_integration_metropolis(unsigned int N, double alpha)
             printf("Electron 2 updated coordinates (%f,%f,%f)\n",
                     x2_m, y2_m, z2_m);
 
-            E_i = local_energy(x1_m, x2_m, y1_m, y2_m, z1_m, z2_m, alpha);
-            fprintf(fp, "%d, %f\n", n_accepted, E_i);
-            E_mean += E_i;
-            E2_mean += pow(E_i, 2.0);
-            n_accepted++;
+            if (i >= burn_period)
+            {
+              //unsigned int start = round(burn_period);
+              printf("Burn period is over, production run:\n");
+              pos_large[n_accepted] = vector_magnitude(x1_m,y1_m,z1_m);
+              printf("pos_large[%u] = %f\n", n_accepted, pos_large[n_accepted]);
+              E_i = local_energy(x1_m, x2_m, y1_m, y2_m, z1_m, z2_m, alpha);
+              fprintf(fp, "%d, %f\n", n_accepted, E_i);
+              fprintf(gp, "%d, %f\n", n_accepted, pos_large[n_accepted]);
+              E_mean += E_i;
+              E2_mean += pow(E_i, 2.0);
+              n_accepted++;
+            }
         }
     }
     fclose(fp);
+    fclose(gp);
 
     E_mean /= n_accepted;
     E2_mean /= n_accepted;
@@ -136,12 +221,13 @@ void mc_integration_metropolis(unsigned int N, double alpha)
     printf("E expectation value:                       %f\n", E_mean);
     printf("sigma_E:                                   %f\n", sigma_E);
     printf("sigma_n:                                   %f\n\n", sigma_n);
+
+    return n_accepted;
 }
 
-double gen_trial_change(double accepted, double rn)
+double gen_trial_change(double accepted, double rn, double d)
 {
     printf("The random number is: %f\n", rn);
-    double d = 2.5;
     return accepted + (d*(rn-0.5));
 }
 
@@ -239,7 +325,8 @@ double local_energy(double x1, double x2,
     return energy;
 }
 
-void calc_autocorrelation(unsigned long length, double time_series[length], double autocorrelation[length/2]){
+void calc_autocorrelation(unsigned long length, double time_series[length], double autocorrelation[length/2])
+{
     long double average_f = 0, average_f2 = 0, average_fifik = 0, var_f, autocsum = 0, k_max = 10000;
     FILE *fp = fopen("./out/autocorrelation.csv", "w");
 
@@ -275,17 +362,21 @@ void calc_autocorrelation(unsigned long length, double time_series[length], doub
     fclose(fp);
 }
 
-void file_to_array(unsigned int length, double data[length]){
+void file_to_array(char *fname, unsigned int length, double data[length])
+{
     FILE *fp;
-    fp = fopen("./dat/MC.txt", "r");
+    fp = fopen(fname, "r");
 
-    for (int i = 0; i < length; i++) {
+    for (int i = 0; i < length; i++)
+    {
         fscanf(fp, "%lf", &data[i]);
     }
     printf("Reading file with: %d lines\n", length);
+    fclose(fp);
 }
 
-void calc_block_average(unsigned long length, double time_series[length], double block_average[length/2]){
+void calc_block_average(unsigned long length, double time_series[length], double block_average[length/2])
+{
     unsigned long num_blocks, block_index;
     double average_f = 0, average_f2 = 0, var_f, bloc_avg, bloc_size_avg, bloc_size_avg_2, var_F, s;
     FILE *fp = fopen("./out/block_averaging.csv", "w");
@@ -327,4 +418,171 @@ void calc_block_average(unsigned long length, double time_series[length], double
         }
     }
     fclose(fp);
+}
+
+/* Copies an slice of an array to a shorter array, to throw away empty elements
+ * @ N - 2 times length of pos_large arrays
+ * @ n_accepted - number of accepted configurations on the Metropolis alg.
+ * @ pos_large[] - positions array, TOO LARGE
+ * @ pos[] - positions array, shorter copy made
+ */
+ void slice_array(unsigned int N, double pos_large[],
+                  unsigned int n_accepted, double *pos)
+{
+  //FILE *fp = fopen("./out/radial.csv", "w");
+  //fprintf(fp, "time, radial\n");
+  for (int i = 0; i < n_accepted; i++)
+  {
+    pos[i] = pos_large[i];
+    //fprintf(fp, "%d, %f, %f\n", i, pos[i], pos_large[i]);
+  }
+  //fclose(fp);
+}
+
+/* Write an array (2nd column) and its index+1 (1st column) to file
+ * @ fname - File's name
+ * @ length - length of the array
+ * @ *array - array of integer values
+ */
+void int_array_to_file(char *fname, int length, int *array)
+{
+  printf("opening...\n");
+  FILE *fp = fopen(fname, "w");
+  printf("opened...\n");
+  fprintf(fp, "bin, frequency\n");
+  printf("header...\n");
+  for (int i = 0; i < length; i++)
+  {
+    printf("%d, %d\n", i+1, array[i]);
+    fprintf(fp, "%d, %d\n", i+1, array[i]);
+  }
+  fclose(fp);
+}
+
+/* Write an array (2nd column) and its index+1 (1st column) to file
+ * @ fname - File's name
+ * @ length - length of the array
+ * @ *array - array of double values
+ */
+void array_to_file(char *fname, int length, double *array)
+{
+  printf("opening...\n");
+  FILE *fp = fopen(fname, "w");
+  printf("opened...\n");
+  fprintf(fp, "bin, frequency\n");
+  printf("header...\n");
+  for (int i = 0; i < length; i++)
+  {
+    printf("%d, %f\n", i+1, array[i]);
+    fprintf(fp, "%d, %f\n", i+1, array[i]);
+  }
+  fclose(fp);
+}
+
+/* Maps a positions array (pos) to an integer positions array (int_pos), to be
+ * used to build a histogram.
+ * @ n_accepted - len_acceptedgth of pos and int_pos arrays
+ * @ pos[] - positions array
+ * @ int_pos[] - integer positions arrays
+ * @ n_bins - number of bins on histogram to build
+ * RETURNS: bin_size (to be used in radial_density_file)
+ */
+double map_to_int(unsigned int n_accepted, double pos[n_accepted],
+                int *int_pos, int n_bins)
+{
+  double maxval = 0;
+
+  for (int i = 0; i < n_accepted; i++)
+  {
+    if (pos[i] > maxval)
+    {
+      maxval = pos[i];
+    }
+  }
+
+  printf("maxval = %f\n", maxval);
+
+  double bin_size = maxval/n_bins;
+
+  for (int i = 0; i < n_accepted; i++)
+  {
+    int_pos[i] = round((pos[i]/bin_size) + 0.5);
+    printf("%f --> %d\n", pos[i], int_pos[i]);
+  }
+  return bin_size;
+}
+
+/* Build a histogram.
+ * @ numvalues - Same value as n_accepted: length of pos and int_pos arrays
+ * @ values[] - Same as pos[]: positions array
+ * SOURCE: https://www.techwalla.com/articles/how-to-create-a-histogram-using-c-programming-code
+ */
+void build_histogram(unsigned int numvalues, int *values)
+{
+  int i = 0;
+
+  // Set maxval
+  int maxval = 0;
+  for (i=0; i<numvalues; i++)
+  {
+    if (values[i] > maxval)
+    {
+      maxval = values[i];
+    }
+  }
+
+  // Set minval
+  int minval = maxval;
+  for (i=0; i<numvalues; i++)
+  {
+    if (values[i] < minval)
+    {
+      minval = values[i];
+    }
+  }
+
+  // Size of frequency array
+  int freqsize = maxval - minval + 1;
+
+  // Declare and initialize frequency array
+  int frequency[freqsize];
+  for (i=0; i<freqsize; i++)
+  {
+    frequency[i] = 0;
+  }
+
+  // Count the frequency of each integer
+  for (i = 0 ; i < numvalues ; i++)
+  {
+    int index = values[i] - minval;
+    frequency[index]++;
+  }
+  printf("minval and maxval: (%d, %d)\n", minval, maxval);
+  int_array_to_file("./out/histogram.csv", freqsize, frequency);
+}
+
+/* Writes a file of radial probability density with the corresponding integer
+ * positions to compare with the sampled histogram.
+ * @ bin_size - Size of bin, returned by map_to_int
+ * @ n_bins - number of bins (for some reason the real val is n_bins+1)
+ * @ Z - Number of electrons
+ */
+void radial_density_file(char *fname, double bin_size, int n_bins, double Z)
+{
+  double rad = 0;
+  double rad_sq;
+  double density = 0;
+  double Z_cb = pow(Z,3.0);
+
+  FILE *fp = fopen(fname, "w");
+  fprintf(fp, "bin, radial density\n");
+
+  for (int i = 0; i < n_bins+1; i++)
+  {
+    rad = bin_size * (i + 0.5);
+    rad_sq = pow(rad, 2.0);
+    density = Z_cb * 4 * rad_sq * exp(-2 * Z * rad);
+    fprintf(fp, "%d, %f\n", i+1, density);
+  }
+  fclose(fp);
 }
--- H2_alejo/task2/src/plotter.py
+++ H2_alejo/task2/src/plotter.py
@@ -5,14 +5,59 @@
 ###############################################################################
 
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
 
-local_energy = np.genfromtxt('./out/local_energy.csv', delimiter=',', skip_header=1)
-plt.figure(figsize=(10,4))
-plt.scatter(local_energy[:, 0], local_energy[:, 1], s=1)
+Z = 'Z = 1.7'
+Zname = '_Z1.7'
+fname1 = 'radial_density'
+fname2 = 'histogram'
+figsname = 'histogram_density_comparison'
+datapath = './out/'
+figpath = './plots/'
+dataext = '.csv'
+figext = '.pdf'
+
+font = {'size'   : 16}
+mpl.rc('font', **font)
+
+radial_density = np.genfromtxt(datapath + fname1 + Zname + dataext,
+                                delimiter=',', skip_header=1)
+histogram = np.genfromtxt(datapath + fname2 + dataext,
+                            delimiter=',', skip_header=1)
+
+x = histogram[:, 0]
+y1 = histogram[:, 1]
+y2 = radial_density[:, 1]
+
+#plt.figure(figsize=(10,4))
+#plt.scatter(radial_density[:, 0]+1, radial_density[:, 1], s=5)
+#plt.plot(radial_density[:, 0]+1, radial_density[:, 1], label = 'Z=2')
 #plt.xlim(2200,2300)
-plt.savefig('./plots/local_energy.pdf')
+#plt.savefig(figpath + fname1 + figext)
+
+#plt.figure(figsize=(10,4))
+#plt.scatter(histogram[:, 0], histogram[:, 1], s=5)
+#plt.plot(histogram[:, 0], histogram[:, 1])
+#plt.xlim(2200,2300)
+#plt.savefig(figpath + fname2 + figext)
+
+fig = plt.figure(figsize=(10,6))
+ax1 = fig.add_subplot(111)
+ax1.plot(x, y1, 'b-')
+ax1.set_ylabel('Histogram', color='b')
+for tl in ax1.get_yticklabels():
+    tl.set_color('b')
+
+ax2 = ax1.twinx()
+ax2.plot(x, y2, 'r-')
+ax2.set_ylabel(r'Radial probability density $\rho(r)$', color='r')
+for tl in ax2.get_yticklabels():
+    tl.set_color('r')
+
+fig.suptitle(Z, fontsize=16)
+plt.savefig(figpath + figsname + Zname + figext)
 
 #local_energy = np.genfromtxt('./out/local_energy.csv', delimiter=',', skip_header=1)
 #fig, ax = plt.subplots(figsize=(10,4))
